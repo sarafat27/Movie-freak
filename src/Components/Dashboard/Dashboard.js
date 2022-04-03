9d2c70adb013067152e50efb28891a6a1e03247a
sarafat27 <sarafat2769@gmail.com>
2022-04-04 01:17:08 +0600

    navbar completed

---
+++ src/Components/Dashboard/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Dashboard.css'
+const Dashboard = () => {
+    return (
+        <div>
+            <h1>This is dashboard</h1>
+        </div>
+    );
+};
+
+export default Dashboard;
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Home.css'
+const Home = () => {
+    return (
+        <div>
+            <h1>Favourite movie</h1>
+        </div>
+    );
+};
+
+export default Home;
