cb8169d4d7957d38c792a55d98f87d26c66c0918
sarafat27 <sarafat2769@gmail.com>
2022-04-04 20:01:41 +0600

    first question added

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -2,8 +2,9 @@ import React from 'react';
 import './Blogs.css'
 const Blogs = () => {
     return (
-        <div>
-            <h1>This is blogs</h1>
+        <div className='blogs'>
+            <h1>(1) What is context API?</h1>
+            <p>Ans: Usually we have to send data from one component to another through props drilling which is inefficient.This problem can be solved by using context API.If we want to send any data from one component to another directly ,we should declare a context in the sender component.And then we have to export it.After that,we should set the context area by wrapping the components with a component named "ContextName.provider".Then, give a value as attribute in this component.At last we can get the data by importing it in the recieving component. </p>
         </div>
     );
 };
