b7b01a8302c2ece678867f28d612f2d2dc8c2fcc
sarafat27 <sarafat2769@gmail.com>
2022-04-05 05:13:36 +0600

    responsiveness added

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -4,9 +4,9 @@ const Card = ({ review }) => {
     const { name, text, rating } = review;
     return (
         <div className='card'>
-            <h4>Name: {name}</h4>
-            <p>Review: {text}</p>
-            <p><small>Rating: {rating}</small></p>
+            <h4 className='card-info'>Name: {name}</h4>
+            <p className='card-info'>{text}</p>
+            <p className='card-info'><small>Rating: {rating}</small></p>
         </div>
     );
 };
